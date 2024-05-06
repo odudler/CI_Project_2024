f1c802f5b7f1cd4fa192680fe64c6d4bbd33a6e2
Oliver Dudler <oliver.dudler@bluewin.ch>
2024-05-06 12:30:49 +0200

    added some util functions for submitting and reading values into data matrix

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import math
 import os
+from sklearn.metrics import mean_squared_error
 
 
 def extract_users_items_predictions(df):
@@ -32,3 +33,26 @@ def create_data_matrix(users, movies, predictions, num_users=10000, num_movies=1
     matrix[users, movies] = predictions.astype(np.float16)
     return matrix
 
+def create_submission(sub_sample_path='data/sampleSubmission.csv', store_path='submissions/default.csv', data_matrix):
+    """
+    Parameters:
+        sub_sample_path (String): Path to sample submission file ("sampleSubmission.csv")
+        store_path (String): Path to store submission file
+        data_matrix (np.ndarray): Data matrix containing the predictions
+    """
+    df = pd.read_csv(sub_sample_path)
+    users, movies, _ = extract_users_items_predictions(df)
+    nrows = df.shape[0]
+
+    df_sub = pd.DataFrame(columns=['row', 'col', 'pred'])
+    df_sub['row'] = users + 1
+    df_sub['col'] = movies + 1
+    data_matrix = np.clip(data_matrix, 1, 5)
+    df_sub['pred'] = data_matrix[users, movies]
+
+    def construct_submission_format(df):
+        return f"r{df['row']:.0f}_c{df['col']:.0f}"
+
+    df_sub['entry'] = df_sub.apply(construct_submission_format, axis=1)
+    df_sub = df_sub.drop(['row', 'col'], axis=1)
+    df_sub.to_csv(store_path, columns=['entry', 'pred'], index=False)
