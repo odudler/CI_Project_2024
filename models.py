1dbc929d4a416ca90d4b571fdff4327a9ee8aa91
Oliver Dudler <oliver.dudler@bluewin.ch>
2024-05-06 17:47:18 +0200

    Added class for SVD++ model, added config file for arguments for each model and in general for training/evaluation, added more utility function (see function descriptions)

---
+++ models.py
@@ -0,0 +1,43 @@
+import math
+import pandas as pd
+import numpy as np
+import tqdm
+from surprise import SVDpp
+from utils import *
+
+class SVDplusplus:
+    def __init__(self, args):
+        self.n_factors = args.svdpp.n_factors
+        self.lr_all = args.svdpp.lr_all
+        self.reg_all = args.svdpp.reg_all
+        self.n_epochs = args.svdpp.n_epochs
+        self.verbose = args.verbose
+        self.random_state = args.random_state
+        
+    def train(self, df):
+        dataset = prepare_data_for_surprise(df)
+        trainset = dataset.build_full_trainset()
+        svdpp = SVDpp(
+            n_factors = self.n_factors,
+            lr_all = self.lr_all,
+            reg_all = self.reg_all,
+            n_epochs = self.n_epochs,
+            verbose = self.verbose,
+            random_state = self.random_state
+        )
+        svdpp.fit(trainset)
+        self.model = svdpp
+
+    def predict(self, df, output_file = None):
+        users, movies, labels = extract_users_items_predictions(df)
+        predictions = np.empty(len(labels))
+        
+        for i in tqdm(range(len(users)), desc = "Prediction Loop"):
+            predictions[i] = self.model.predict(users[i], movies[i], verbose=self.verbose)
+
+        if output_file is not None:
+            create_submission_from_array(predictions)
+        
+        print(f"RMSE for {self.__class__.__name__}: {calculate_rmse(predictions, labels)}")
+
+
--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import pandas as pd
 import math
 import os
 from sklearn.metrics import mean_squared_error
+from surprise import Reader, Dataset
+import argparse
+import yaml
 
 
 def extract_users_items_predictions(df):
@@ -19,6 +22,22 @@ def extract_users_items_predictions(df):
     predictions = df.Prediction.values
     return users, movies, predictions
 
+def prepare_data_for_surprise(df):
+    """
+    Parameters:
+        df (pd.DataFrame): DataFrame to process, containing users, movie and prediction information
+    Returns:
+        dataset (surprise.Dataset): Dataset containing the training data
+    """
+    users, movies, predictions = extract_users_items_predictions(df)
+    df_surprise = pd.DataFrame({
+        'itemID': movies,
+        'userID': users,
+        'rating': predictions
+    })
+    return Dataset.load_from_df(df_surprise, reader=Reader(rating_scale=(1,5)))
+
+
 def create_data_matrix(users, movies, predictions, num_users=10000, num_movies=1000):
     """
     Parameters:
@@ -33,7 +52,7 @@ def create_data_matrix(users, movies, predictions, num_users=10000, num_movies=1
     matrix[users, movies] = predictions.astype(np.float16)
     return matrix
 
-def create_submission(sub_sample_path='data/sampleSubmission.csv', store_path='submissions/default.csv', data_matrix):
+def create_submission_from_matrix(data_matrix, sub_sample_path='data/sampleSubmission.csv', store_path='submissions/default.csv'):
     """
     Parameters:
         sub_sample_path (String): Path to sample submission file ("sampleSubmission.csv")
@@ -56,3 +75,70 @@ def create_submission(sub_sample_path='data/sampleSubmission.csv', store_path='s
     df_sub['entry'] = df_sub.apply(construct_submission_format, axis=1)
     df_sub = df_sub.drop(['row', 'col'], axis=1)
     df_sub.to_csv(store_path, columns=['entry', 'pred'], index=False)
+
+def create_submission_from_array(predictions, sub_sample_path='data/sampleSubmission.csv', store_path='submissions/default.csv'):
+    """
+    Parameters:
+        sub_sample_path (String): Path to sample submission file ("sampleSubmission.csv")
+        store_path (String): Path to store submission file
+        predictions (np.array): Array containing rating predictions
+    """
+    hi = "hello"
+    df = pd.read_csv(sub_sample_path)
+    users, movies, _ = extract_users_items_predictions(df)
+    nrows = df.shape[0]
+
+    df_sub = pd.DataFrame(columns=['row', 'col', 'pred'])
+    df_sub['row'] = users + 1
+    df_sub['col'] = movies + 1
+    data_matrix = np.clip(data_matrix, 1, 5)
+    df_sub['pred'] = predictions
+
+    def construct_submission_format(df):
+        return f"r{df['row']:.0f}_c{df['col']:.0f}"
+
+    df_sub['entry'] = df_sub.apply(construct_submission_format, axis=1)
+    df_sub = df_sub.drop(['row', 'col'], axis=1)
+    df_sub.to_csv(store_path, columns=['entry', 'pred'], index=False)
+
+
+def calculate_rmse(preds, labels):
+    """
+    Parameters:
+        preds (np.array): predictions of ratings
+        labels (np.array): true ratings by users
+    
+    Returns:
+        rmse_loss (float): Rooted mean squared error
+    """
+    return math.sqrt(mean_squared_error(preds, labels))
+
+def read_config(config_path):
+    """
+    TODO: Extend this function for every added model!
+    Parameters:
+        config_path (String): path to the config to load
+    
+    Returns:
+        args: argument Namespace containing the config arguments 
+    """
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file.read())
+    
+    args = argparse.Namespace()
+
+    #General training arguments
+    args.random_state = config['args']['training']['random_state']
+    args.n_users = config['args']['training']['n_users']
+    args.n_movies = config['args']['training']['n_movies']
+    args.test_set_size = config['args']['training']['test_set_size']
+    args.verbose = config['args']['training']['verbose']
+
+    #SVDplusplus arguments
+    args.svdpp = argparse.Namespace()
+    args.svdpp.n_factors = config['args']['SVDplusplus']['n_factors']
+    args.svdpp.lr_all = config['args']['SVDplusplus']['lr_all']
+    args.svdpp.n_epochs = config['args']['SVDplusplus']['n_epochs']
+    args.svdpp.reg_all = config['args']['SVDplusplus']['reg_all']
+
+    return args
