b70f10161870d7560521f27a7392e23f5bb27b27
Oliver Dudler <oliver.dudler@bluewin.ch>
2024-05-10 12:22:36 +0200

    cleaned up the ncf model to have similar code to the other models, cleaned up the config and grid_search_config

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+import itertools as IT
 from tqdm import tqdm
 from surprise import SVDpp, SVD, KNNWithZScore
 from utils import (
@@ -132,48 +134,43 @@ class NeuralCF:
         self.n_movies = args.n_movies
 
     def train(self, df):
+        #Need to create temporary file with data as NCFDataset expects file
         self.mean, self.std = prepare_data_for_recommender(df, self.n_users, self.n_movies, 'temp.csv')
-
         data = NCFDataset(train_file='temp.csv', test_file=None, seed=self.random_state, binary=False,
                           overwrite_test_file_full=False, n_neg_test=0, n_neg=0)
+        
         model = NCF (
             n_users=self.n_users, 
             n_items=self.n_movies,
-            model_type="NeuMF",
-            n_factors=self.ncf_args.n_factors,
-            layer_sizes=[16,8,4],
-            n_epochs=self.ncf_args.n_epochs,
-            batch_size=self.ncf_args.batch_size,
-            learning_rate=self.ncf_args.lr,
+            **self.ncf_args.__dict__,
             verbose=self.verbose,
             seed=self.random_state
-        )       
+        )
+
         model.fit(data)
         self.model = model
 
+        #Delete temporary file again
+        if os.path.isfile('temp.csv'):
+            os.remove('temp.csv')
+
     def predict(self, df, output_file=None, return_loss=False):
         users, movies, labels = extract_users_items_predictions(df)
-        # predictions = np.empty(len(labels))
+        predictions = np.empty(len(labels))
 
         reconstructed = np.zeros((self.n_users, self.n_movies))
-        for i in range(self.n_users):
-            for j in range(self.n_movies): #TODO: don't need to predict all here, just the ones in the submission file
-                reconstructed[i,j] = self.model.predict(i,j)
+        idx = 0
+        for i, j in IT.izip(users, movies):
+            reconstructed[i,j] = predictions[idx] = self.model.predict(i,j)
+            idx+=1
         reconstructed = denormalize_columns(reconstructed, self.mean, self.std)
-
-        # for i in tqdm(range(len(users)), desc="Prediction Loop"):
-        #     predictions[i] = self.model.predict(
-        #         users[i], movies[i], verbose=self.verbose
-        #     ).est
 
         if output_file is not None:
             create_submission_from_matrix(reconstructed, users, movies, output_file)
 
-        print("loss returning not implemented yet")
-        return 0
-        # loss = calculate_rmse(predictions, labels)
-        # if return_loss:
-        #     return loss
-        # else:
-        #     print(f"RMSE for {self.__class__.__name__}: {loss}")
-        #     return None
+        loss = calculate_rmse(predictions, labels)
+        if return_loss:
+            return loss
+        else:
+            print(f"RMSE for {self.__class__.__name__}: {loss}")
+            return None
